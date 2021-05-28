e1fbbaf76d898aaa5950d286f59923f402ea60bf
Marina <marina_cape@hotmail.com>
2021-05-28 20:53:19 +0200

    Test add of the calculator

---
+++ Calculadora/src/test/CalculatorTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	//Class to test
+	private Calculator calculator;
+	
+	/**
+	 * Initialice requirements objets
+	 * */
+	@Before
+	public void setUp() {
+		calculator = new Calculator();
+	}
+	
+	@Test
+	public void testAdd() {
+		//arrange
+		int firstNumber = 2;
+		int secondNumber = 5;
+		int expectedResult = 7;
+		//act
+		int realResult = calculator.add(firstNumber, secondNumber);
+		//assert
+		Assert.assertEquals(expectedResult, realResult);
+	}
+}
