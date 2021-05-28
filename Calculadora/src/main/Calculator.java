c102eef3b7e727503dcb834a5d4b28757240c41e
Marina <marina_cape@hotmail.com>
2021-05-28 20:57:02 +0200

    Implementation add method in Calculator object

---
+++ Calculadora/src/main/Calculator.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Calculator {
+	
+	public Calculator() {}
+	
+	public int add(int firstNumber, int secondNumber) {
+		return firstNumber + secondNumber;
+	}
+	
+}
--- Calculadora/src/test/CalculatorTest.java
+++ Calculadora/src/test/CalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import main.Calculator;
+
 public class CalculatorTest {
 	
 	//Class to test
